967f012d41e63c6ccf06a49f946a921b621d9f8e
andrd-jhon <jv-andrade02@outlook.com>
2025-10-19 23:27:17 -0300

    Teste utilizando import de imagem.

--- raissa-andrade-fisio/src/app/page.tsx
+++ raissa-andrade-fisio/src/app/page.tsx
@@ -1,6 +1,8 @@
 
 import BarraSuperior from '@/widgets/BarraSuperior'
 import React from 'react'
+import Image from "next/image"
+import imgPerfilCard from '../assets/imgCardPerfil.jpg'
 
 
 export default function Home() {
@@ -9,19 +11,24 @@ export default function Home() {
   return (
     <div className="">
       <BarraSuperior></BarraSuperior>
-      <h1 className='flex justify-center w-screen'>FISIOTERAPIA PÉLVICA E OBSTÉTRICA</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem iure
-        assumenda laudantium. Dolore ratione ea obcaecati natus ut quisquam,
-        expedita saepe nam quaerat ab? Magni odio nobis dignissimos in earum
-        doloribus fuga, pariatur quam ab id? Laboriosam odit dignissimos
-        aspernatur quae obcaecati quidem iusto explicabo. Iusto aliquam at rem
-        est architecto fuga maxime officia quas debitis excepturi. In dolores
-        qui, deserunt, corrupti vel reprehenderit velit ab fuga amet quasi
-        minima, cumque perspiciatis beatae nihil. Veritatis sed itaque facilis
-        non exercitationem cupiditate ducimus, nobis eum doloremque sit vitae?
-        Ex, nam?
-      </p>
+      <div className='flex justify-between'>
+        <Image src={imgPerfilCard} alt={'Imagem da fisioterapeuta Raissa Andrade.'} width={100} height={100}></Image>
+        <div className='w-100'>
+          <h1 className=''>FISIOTERAPIA PÉLVICA E OBSTÉTRICA</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem iure
+            assumenda laudantium. Dolore ratione ea obcaecati natus ut quisquam,
+            expedita saepe nam quaerat ab? Magni odio nobis dignissimos in earum
+            doloribus fuga, pariatur quam ab id? Laboriosam odit dignissimos
+            aspernatur quae obcaecati quidem iusto explicabo. Iusto aliquam at rem
+            est architecto fuga maxime officia quas debitis excepturi. In dolores
+            qui, deserunt, corrupti vel reprehenderit velit ab fuga amet quasi
+            minima, cumque perspiciatis beatae nihil. Veritatis sed itaque facilis
+            non exercitationem cupiditate ducimus, nobis eum doloremque sit vitae?
+            Ex, nam?
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
